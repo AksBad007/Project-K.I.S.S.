bdd67e8dd6822f584b92b63c59bab7963f9654fc
Akshat <arora.akshat007@gmail.com>
2021-06-14 22:42:54 +0530

    All Done

--- Changes.py
+++ Changes.py
@@ -1,10 +1,11 @@
+import os
 from flask import Blueprint, render_template, redirect, request, url_for
 from pymongo import MongoClient
 
 Changelog = Blueprint('Changelog', __name__, url_prefix='/changes')
 
 client = MongoClient("mongodb+srv://"+str(os.environ.get(DB_USER))+":"+str(os.environ.get(DB_PASSWORD))+"@cluster0.azvnt.mongodb.net/kissDB?retryWrites=true&w=majority")
-db = client.kissDB
+db = client["kissDB"]
 
 @Changelog.route('/', methods=['GET'])
 def changePage():
